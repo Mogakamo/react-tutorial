79fbf1f20b2ffa930e3f32f0dfe67eebd03a439a
Amos Mogaka <mogaka.amo254@gmail.com>
2021-01-23 21:33:17 +0300

    Commit

--- src/App.js
+++ src/App.js
@@ -23,6 +23,15 @@ class App extends Component {
         ],
 
     }
+    removeCharacter = (index) => {
+        const {characters} = this.state
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index
+            }), 
+        })
+    }
 }
 
 export default App

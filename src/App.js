16b0ebbb1cc73d30680e8979df0900aed3a4617d
Amos Mogaka <mogaka.amo254@gmail.com>
2021-01-23 21:23:23 +0300

    Commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import React, {Component} from 'react'
 import Table from './Table'
 
 class App extends Component {
-    render() {
-        return (
-            <div className="container">
-                <Table />
-            </div>
-        )
+    state = {
+        characters: [],
     }
 }
 
